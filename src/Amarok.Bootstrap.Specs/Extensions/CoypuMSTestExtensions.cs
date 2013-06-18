c69e8c7e0cfaf269463842bbd4f171e3424a9bc4
Bernardo Bosak de Rezende <bernardobrezende@gmail.com>
2013-06-18 12:01:02 -0300

    adding extension for "browser.ShouldHaveContent"

---
+++ src/Amarok.Bootstrap.Specs/Extensions/CoypuMSTestExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Coypu;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Coypu
+{
+    public static class CoypuMSTestExtensions
+    {
+        public static void ShouldHaveContent(this BrowserSession browserSession, string content)
+        {
+            Assert.IsTrue(browserSession.HasContent(content));
+        }
+    }
+}
