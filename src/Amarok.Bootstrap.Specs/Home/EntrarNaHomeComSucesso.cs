61f48828542fcb587271c78a622f6c6475c96966
Bernardo Bosak de Rezende <bernardobrezende@gmail.com>
2013-06-18 11:22:26 -0300

    adding initial structure with specflow + asp.net mvc 4 + mstest.

---
+++ src/Amarok.Bootstrap.Specs/Home/EntrarNaHomeComSucesso.cs
@@ -0,0 +1,20 @@
+﻿using TechTalk.SpecFlow;
+
+namespace Amarok.Bootstrap.Specs.Home
+{
+    [Binding]
+    public class EntrarNaHomeComSucesso
+    {
+        [When(@"eu acessar a home do site")]
+        public void QuandoEuAcessarAHomeDoSite()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"preciso ver o texto ""(.*)""")]
+        public void EntaoPrecisoVerOTexto(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
---
+++ src/Amarok.Bootstrap.Specs/Home/Home.feature
@@ -0,0 +1,9 @@
+﻿#language: pt-br
+
+Funcionalidade: Home
+	Como usuário do sistema
+	Preciso de uma página de boas vindas.
+
+Cenário: Entrar na home com sucesso
+	Quando eu acessar a home do site
+	Então preciso ver o texto "Seja bem vindo"
